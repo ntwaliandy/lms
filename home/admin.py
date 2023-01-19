7a822a5894a32cf25f50f33ed03d8c6d12f85102
ntwaliandy <ntwaliandy90@gmail.com>
2023-01-19 10:17:27 +0300

    changed permit apply admin view

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Apply, GroupApply, Support, PermitApply
+from django.contrib import admin
+
+
+class permitAdmin(admin.ModelAdmin):
+    list_display = ("permit_id", "first_name", "last_name", "service", "deposits", "balance", "final_amount")
+    search_fields = ["permit_id", "first_name", "last_name", "service", "deposits", "balance", "final_amount"]
+
 # Register your models here.
 admin.site.register(Apply)
 admin.site.register(GroupApply)
 admin.site.register(Support)
-admin.site.register(PermitApply)
+admin.site.register(PermitApply, permitAdmin)
