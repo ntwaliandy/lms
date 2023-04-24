e0f82aef019d8585919d6368d0b90f4eeb5e7299
ntwaliandy <ntwaliandy90@gmail.com>
2023-04-24 19:24:13 +0300

    added admin filters

--- loan/admin.py
+++ loan/admin.py
@@ -6,7 +6,13 @@ class permitAddPaymentAdmin(admin.ModelAdmin):
     list_display = ("permit_id", "payment_fee", "phone_number", "status", "reference", "date", "transaction_id")
     search_fields = ["permit_id", "payment_fee", "phone_number", "status", "reference", "date", "transaction_id"]
 
+class BodaBodaAdmin(admin.ModelAdmin):
+    list_display = ("boda_guy_firstName", "boda_guy_lastName", "boda_numberPlate", "weekly_pay", "deposits", "balance", "final_amount")
+    search_fields = ["boda_guy_firstName", "boda_guy_lastName", "boda_numberPlate", "weekly_pay", "deposits", "balance", "final_amount"]
 
+class BodaWeeklyAdmin(admin.ModelAdmin):
+    list_display = ("boda_id", "boda_firstName", "boda_lastName", "payment_fee", "date")
+    search_fields = ["boda_id", "boda_firstName", "boda_lastName", "payment_fee", "date"]
 
 # Register your models here.
 admin.site.register(AddPayment)
@@ -14,5 +20,5 @@ admin.site.register(GroupAddPayment)
 admin.site.register(Replies)
 admin.site.register(AddPermitPayment, permitAddPaymentAdmin)
 admin.site.register(FileUpload)
-admin.site.register(BodaApply)
-admin.site.register(BodaWeeklyPay)
+admin.site.register(BodaApply, BodaBodaAdmin)
+admin.site.register(BodaWeeklyPay, BodaWeeklyAdmin)
